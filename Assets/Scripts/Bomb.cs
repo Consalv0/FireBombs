f604fa4dc2559bbe627774c498c3a370a6c49941
Consalvo <gonzaloceciliano@gmail.com>
2017-05-23 10:43:24 -0500

    Added Explosion

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -3,8 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour {
+	public float ExplosionRadius;
+	public float ExplosionForce;
+	public float LifeSpawn;
+
+	void Update() {
+		LifeSpawn -= 0.5f;
+		if(LifeSpawn <= 0) {
+			Collider[] colliders = Physics.OverlapSphere(transform.position, ExplosionRadius);
+			foreach(Collider coll in colliders) {
+				if (coll.GetComponent<Rigidbody>() == null) continue;
+				coll.GetComponent<Rigidbody>().AddExplosionForce(ExplosionForce, transform.position, ExplosionRadius, 1, ForceMode.Impulse);
+			}
+			Destroy(transform.parent.gameObject);
+		}
+	}
+
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.GetComponent<LinearMovement>() != null) {
+			Collider[] colliders = Physics.OverlapSphere(transform.position, ExplosionRadius);
+			foreach(Collider coll in colliders) {
+				if (coll.GetComponent<Rigidbody>() == null) continue;
+				coll.GetComponent<Rigidbody>().AddExplosionForce(ExplosionForce, transform.position, ExplosionRadius, 1, ForceMode.Impulse);
+			}
 			Destroy(other.gameObject);
 			Destroy(transform.parent.gameObject);
 		}
--- Assets/Scripts/NewBomb.cs
+++ Assets/Scripts/NewBomb.cs
@@ -4,25 +4,35 @@ using UnityEngine;
 
 public class NewBomb : MonoBehaviour {
 	public float Distance;
-	public float RangeX;
 	public GameObject Spaceship;
 	public GameObject Bomb;
+	public float minLife;
+	public float maxLife;
 
 	GameObject bomb = null;
+	float lifeSpawn = 0;
+	float x, y, z;
 
 	void LateUpdate() {
-		if (Input.GetButtonUp("Fire1")) { //&& bomb == null) {
-			var x = (Input.mousePosition.x / Screen.width) * 2;
-			var z = Input.mousePosition.y / Screen.height;
-			var y = z * 4;
-			x = (x - 1) * RangeX;
+		if (Input.GetButton("Fire1")) { //&& bomb == null) {
+			lifeSpawn++;
+			x = (Input.mousePosition.x / Screen.width) * 2;
+			z = Input.mousePosition.y / Screen.height;
+			y = z * 10;
+			x = (x - 1) * Distance;
 			z = z * Distance;
-			newBomb(new Vector3(x, y, z));
+		}
+
+		if (Input.GetButtonUp("Fire1")) { //&& bomb == null) {
+			lifeSpawn = lifeSpawn <= minLife ? minLife : lifeSpawn >= maxLife ? maxLife : lifeSpawn;
+			newBomb(new Vector3(x, y, z), lifeSpawn);
+			lifeSpawn = 0;
 		}
 	}
 
-	void newBomb(Vector3 velocity) {
+	void newBomb(Vector3 velocity, float lifeSpawn) {
 		bomb = Instantiate(Bomb, Spaceship.transform.position, Quaternion.identity);
+		bomb.GetComponentInChildren<Bomb>().LifeSpawn = lifeSpawn;
 		bomb.GetComponent<Rigidbody>().velocity = velocity;
 		bomb.GetComponent<Rigidbody>().AddTorque(new Vector3(10, 10, 0));
 	}
