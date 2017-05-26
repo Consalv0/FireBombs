ada67624ee8bcd3e013869527d19d9d618e5a125
Consalvo <gonzaloceciliano@gmail.com>
2017-05-25 23:26:53 -0500

    Progress in Camera

---
+++ Assets/Scripts/ControllerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ControllerMovement : MonoBehaviour {
+	public float MovementSpeed = 10f;
+	public float SprintMultiplier = 1.6f;
+	public float Sensitivity = 5f;
+	public float Smoothing = 2f;
+
+	Vector2 deltaMovement;
+	Vector2 smoothV;
+	float sprint;
+	float x = 0;
+	float z = 0;
+
+	void Start () {
+		sprint = SprintMultiplier;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void LateUpdate () {
+		// Translation
+		if (Input.GetButtonDown("Sprint")) {
+			if (sprint != 1)
+				sprint = 1;
+			else
+				sprint = SprintMultiplier;
+		}
+
+		x = Input.GetAxis("Left Horizontal") * Time.deltaTime * MovementSpeed * SprintMultiplier / sprint;
+		z = Input.GetAxis("Left Vertical") * Time.deltaTime * MovementSpeed * SprintMultiplier / sprint;
+
+		// Rotation
+		var mouseDirection = new Vector2(Input.GetAxis("Right Horizontal"), Input.GetAxis("Right Vertical"));
+
+		mouseDirection = Vector2.Scale(mouseDirection, new Vector2(Sensitivity * Smoothing, Sensitivity * Smoothing));
+		smoothV.x = Mathf.Lerp(smoothV.x, mouseDirection.x, 1 / Smoothing);
+		smoothV.y = Mathf.Lerp(smoothV.y, mouseDirection.y, 1 / Smoothing);
+		deltaMovement += smoothV;
+
+		// Assign Values
+    transform.Translate(x, 0, z);
+		transform.rotation = Quaternion.Euler(-deltaMovement.y, deltaMovement.x, 0);
+	}
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+	public Transform Target;
+	public float RotationX;
+	public float RotationY;
+	public float RotationZ;
+	public float Distance = 20;
+	public float RotationSpeed = 10f;
+
+	void Start() {
+		transform.rotation *= Quaternion.AngleAxis(RotationX, Vector3.left);
+    transform.rotation *= Quaternion.AngleAxis(RotationY, Vector3.up);
+	  transform.rotation *= Quaternion.AngleAxis(RotationZ, Vector3.forward);
+	}
+
+	void LateUpdate() {
+	}
+}
