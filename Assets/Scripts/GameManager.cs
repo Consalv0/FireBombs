1b0db0f158d448ac0d461a1d2ca9050d63eb09c5
Consalvo <gonzaloceciliano@gmail.com>
2017-06-21 00:26:37 -0500

    GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+			Application.Quit();
+
+		if (player == null) {
+			SceneManager.UnloadSceneAsync("MainWorld");
+			SceneManager.LoadScene("MainWorld");
+		}
+	}
+}
